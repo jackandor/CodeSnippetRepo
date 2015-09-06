9596628bd749799b9e8af4891a77085e4db5992b
jack cao <19173347@qq.com>
2015-09-07 00:05:59 +0800

    Add gen_name_list function.

--- C_Struct_Analyzer/main.c
+++ C_Struct_Analyzer/main.c
@@ -50,6 +50,36 @@ void print_struct(struct struct_def *s)
   }
 }
 
+char buffer[10 * 1024 * 1024];
+int offset;
+#define print(fmt, ...) offset += sprintf(&buffer[offset], fmt, ##__VA_ARGS__)
+
+int indent;
+
+void print_indent()
+{
+  for(int i = 0; i < indent; i++)
+    print("  ");
+}
+
+#define println(fmt, ...) do { \
+  print_indent(); \
+  print(fmt, ##__VA_ARGS__); \
+  print("\n"); \
+} while(0)
+
+void gen_name_list(struct var_def *v)
+{
+  println("const char *name_list[] = {");
+  indent++;
+  while(v != NULL) {
+    println("\"%s\",", v->name);
+    v = v->next;
+  }
+  indent--;
+  println("};");
+}
+
 int main(int argc, char *argv[])
 {
   if(argc < 2) {
@@ -59,5 +89,11 @@ int main(int argc, char *argv[])
   yyin = fopen(argv[1], "r");
   yyparse();
   print_struct(head);
+  struct struct_def *s = head;
+  while(s != NULL) {
+    gen_name_list(s->head);
+    s = s->next;
+  }
+  printf("%s", buffer);
   return 0;
 }
