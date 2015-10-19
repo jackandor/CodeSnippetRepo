3c49c61649bad98a2a042a14f65e24c3938b8926
jack cao <19173347@qq.com>
2015-10-19 16:07:10 +0800

    Add find_struct(...) and size_of_struct(...) function

--- C_Struct_Analyzer/table.c
+++ C_Struct_Analyzer/table.c
@@ -35,3 +35,44 @@ void free_all(struct struct_def *h)
 }
 
 struct struct_def *head;
+
+struct struct_def *find_struct(const char *name)
+{
+    struct struct_def *p = head;
+    while(strcmp(name, p->name)) p = p->next;
+    return p;
+}
+
+int size_of_struct(struct struct_def *sd)
+{
+    int ret = 0;
+    struct var_def *v = sd->head;
+    while(!v) {
+        int sz = 0;
+        if(v->pointer)
+            sz = 4;
+        else {
+            switch(v->type) {
+            case CHARTYPE:
+                sz = 1;
+                break;
+            case SHORTTYPE:
+                sz = 2;
+                break;
+            case INTTYPE:
+            case LONGTYPE:
+            case FLOATTYPE:
+                sz = 4;
+                break;
+            case DOUBLETYPE:
+                sz = 8;
+                break;
+            }
+        }
+        if(v->array_size)
+            sz *= v->array_size;
+        ret += sz;
+        v = v->next;
+    }
+    return ret;
+}
